14e30e1e04d0615ac593e6e0e04ba3749d4c0c43
Ethan <mrethanvlogsandgames@gmail.com>
2022-05-14 14:35:02 -0500

    Adventure

--- README.md
+++
@@ -1,4 +0,0 @@
-# pepe the frog
-this is pepe the frog, a discord bot
-i don't know what I'm doing right now
-i just like coding"# pepe-the-frog" 
--- commands/adventure.js
+++ commands/adventure.js
@@ -1,15 +1,34 @@
 // discord.js template
 const discord = require("discord.js")
-const {SlashCommandBuilder} = require("@discordjs/builders")
+const { SlashCommandBuilder } = require("@discordjs/builders")
 module.exports = {
     data: new SlashCommandBuilder()
         .setName("adventure")
         .setDescription("Choose an adventure."),
     /**
-     * @param {CommandInteraction} interaction
-     * @param {Client} client
+     * @param {discord.CommandInteraction} interaction
+     * @param {discord.Client} client
      */
     async execute(interaction, client) {
-        
+        await interaction.deferReply()
+        const row = new discord.MessageActionRow()
+            .addComponents(new discord.MessageSelectMenu({
+                customId: "adventure",
+                options: [{
+                    label: "Space",
+                    description: "You are going to space to replace that American flag with your own."
+                }]
+            }))
+
+        const embed = new discord.MessageEmbed({
+            title: "Adventure",
+            description: "Space",
+            color: "GREEN",
+            author: {
+                name: `${interaction.user.tag} is going on an adventure`,
+                iconURL: interaction.user.displayAvatarURL()
+            }
+        })
+        await interaction.reply({ embeds: [embed] })
     }
 }
